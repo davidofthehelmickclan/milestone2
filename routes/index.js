6cddee5cdab998046921d02a40d4b0ed84dac680
davidofthehelmickclan <davidofthehelmickclan@yahoo.com>
2016-02-26 12:12:51 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+var entries [{ slug: "how to pass class", body:"com to class"}]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Today I Learned' });
