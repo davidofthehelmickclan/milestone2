71c8a9a0ac1a73a542660eedbbd0dc0526eaf84b
davidofthehelmickclan <davidofthehelmickclan@yahoo.com>
2016-03-04 23:07:11 -0700

    Update til.js

--- routes/til.js
+++ routes/til.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-var entries = [
+var til = [
   {slug:"how to pass class", body: "come to class. do your homework", created_at: "some date"},
   {slug:"how to fail class", body: "play video games all day", created_at: "some date"}
 ];
 
 /* READ all: GET til listing. */
 router.get('/', function(req, res, next) {
-  res.render('til/index', { title: 'Blog', entries: entries });
+  res.render('til/index', { title: 'Blog', til: til });
 });
 
 /* CREATE entry form: GET /til/new */
@@ -19,7 +19,7 @@ router.get('/new', function(req, res, next) {
 /*CREATE entry: POST /til/ */
 router.post('/', function(req, res, next) {
   til.push(req.body);
-  res.render('til/index', { title: 'Blog', entries: entries });
+  res.render('til/index', { title: 'Blog', til: til });
 });
 
 /* UPDATE entry form: GET /til/1/edit */
@@ -28,31 +28,31 @@ router.get('/:id/edit', function(req, res, next) {
   {
     title: 'Update an entry',
     id: req.params.id,
-    entry: entries[req.params.id]
+    entry: til[req.params.id]
   });
 });
 
 /* UPDATE entry: POST /til/1 */
 router.post('/:id', function(req, res, next) {
-  entries[req.params.id] = req.body;
+  til[req.params.id] = req.body;
   res.render('til/index',
   {
     title: 'Update an entry',
-    entries: entries
+    til: til
   });
 });
 
 /* DELETE entry: GET /til/1/delete  */
 router.get('/:id/delete', function(req, res, next) {
   var id = req.params.id
-  entries = entries.slice(0,id).concat(entries.slice(id+1, entries.length));
-  res.render('til/index', { title: 'Blog', entries: entries });
+  til = til.slice(0,id).concat(til.slice(id+1, til.length));
+  res.render('til/index', { title: 'Blog', til: til });
 });
 
 /* THIS NEEDS TO BE LAST or /new goes here rather than where it should */
 /* READ one entry: GET /til/0 */
 router.get('/:id', function(req, res, next) {
-  res.render('til/entry', {title: "a entry", entry: entries[req.params.id]});
+  res.render('til/entry', {title: "a entry", entry: til[req.params.id]});
 });
 
 module.exports = router;
