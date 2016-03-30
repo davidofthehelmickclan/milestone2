05391b40077d418c5258a48953687e14cbc0d643
davidofthehelmickclan <davidofthehelmickclan@yahoo.com>
2016-03-29 22:04:51 -0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var crypto = require('crypto');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,7 +11,6 @@ router.get('/login', function(req, res, next) {
 });
 
 router.post('/login', function(req, res, next) {
-  var sha1sum = crypto.createHash('sha1');
 
   req.db.driver.execQuery(
     'SELECT * FROM users WHERE email=?;',
@@ -23,10 +21,7 @@ router.post('/login', function(req, res, next) {
         console.log(err);
       }
 
-      sha1sum.update(req.body.password);
-      var hashed_input = sha1sum.digest('hex');
-
-      if(hashed_input === data[0].password) //DONT Do this is other projects!!!
+      if(req.body.password === data[0].password) //DONT Do this is other projects!!!
       {
         res.cookie('username', data[0].name);
         res.redirect('/til/');
